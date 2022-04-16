f418220afadcd29008a4726e81c92f8b60eeac82
FUJIMURA Gaku <fujimura@allm.net>
2022-04-17 08:49:18 +0900

    画像サイズ変更

--- tools/cut_image.rb
+++ tools/cut_image.rb
@@ -3,7 +3,7 @@ require 'fileutils'
 require 'RMagick'
 
 InputPath = './output1'
-SavePath = '../images'
+SavePath = './output2'
 FileUtils.mkdir_p SavePath unless Dir.exist? SavePath
 
 q_number = 0
@@ -13,17 +13,17 @@ Dir.glob(File.join(InputPath, '*.png')).sort.each do |file|
   image = Magick::ImageList.new(file).first
   width = image.columns
   height = image.rows
-  out_width = (width - 1) / 2
-  out_height = (height - 1) / 2
+  out_width = (width - 1) / 2 - 1
+  out_height = (height - 1) / 2 - 20
 
   q_number += 1
   q1 = image.crop(0, 0, out_width, out_height)
   q1.write(Pathname.new(SavePath).join("#{q_number}q.png"))
-  a1 = image.crop(width - out_width + 1, 0, out_width, out_height)
+  a1 = image.crop(width - out_width, 0, out_width, out_height)
   a1.write(Pathname.new(SavePath).join("#{q_number}a.png"))
   q_number += 1
   q2 = image.crop(0, height - out_height, out_width, out_height)
   q2.write(Pathname.new(SavePath).join("#{q_number}q.png"))
-  a2 = image.crop(width - out_width + 1, height - out_height, out_width, out_height)
+  a2 = image.crop(width - out_width, height - out_height, out_width, out_height)
   a2.write(Pathname.new(SavePath).join("#{q_number}a.png"))
 end
